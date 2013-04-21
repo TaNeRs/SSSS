d13cf3377185ec84447b24665c79f3b960d1cb45
unknown <rcyeh@m11ximilian.(none)>
2013-04-20 23:43:29 -0700

    added class for removing stop words and Snowball stemming- currently outputs to console

--- SSSS/Form1.cs
+++ SSSS/Form1.cs
@@ -8,13 +8,13 @@ using System.Text;
 using System.Windows.Forms;
 using System.Configuration;
 using com.reddit.api;
+using Iveonik.Stemmers;
+using System.Text.RegularExpressions;
 
 namespace SSSS {
     public partial class Form1 : Form {
 
         List<Post> postList;
-
-        Dictionary<string, int> wordCountDict = new Dictionary<string, int>();
 
         public Form1() {
             InitializeComponent();
@@ -37,72 +37,21 @@ namespace SSSS {
 
         private void PostListView_SelectedIndexChanged(object sender, EventArgs e) {
             string postID = (sender as ListControl).SelectedValue.ToString();
+            string opText = ((sender as ListBox).SelectedItem as Post).SelfText;
+            PostPreview.Text = opText;
 
-            PostPreview.Text = ((sender as ListBox).SelectedItem as Post).SelfText;
-
+            opText = WordStopper.RemoveStopWords(opText);
+            opText = Regex.Replace(opText, @"[^\w\s+]", "");
+            string[] opArr = opText.Split(null);
+            TestStemmer(new EnglishStemmer(), opArr);
         }
 
-        //*********************************Vincent's Test Code****************************/
-
-        //List<Post> lst = new List<Post>();
-        //lst.Add(postList[0]);
-        //lst.Add(postList[12]);
-        //Dictionary<string, int> wordCountDict2 = new Dictionary<string, int>();
-        //Utilities.CountUpWords(lst, wordCountDict2);
-
-        //int ct = 26;
-        //foreach (KeyValuePair<string, int> itm in wordCountDict2) {
-        //    if (ct > 0) {
-        //        wordCountDict.Add(itm.Key, itm.Value);
-        //        ct--;
-        //    }
-        //    else {
-        //        break;
-        //    }
-        //}
-
-        ////Utilities.CountUpWords(postList, wordCountDict);
-        //int k = 1;
-        //foreach (KeyValuePair<string, int> itm in wordCountDict) {
-        //    System.Console.WriteLine("Item# " + k + " - Key: " + itm.Key + ", Value: " + itm.Value);
-        //    k++;
-        //}
-        //List<Dictionary<string, int>> folds = Utilities.NFold(5, wordCountDict);
-        //for (int i = 0; i < folds.Count; i++) {
-        //    System.Console.WriteLine();
-        //    System.Console.WriteLine();
-        //    System.Console.WriteLine("****************************************FOLD " + (i + 1).ToString() + " STARTS HERE!!!!!");
-        //    int j = 1;
-        //    foreach (KeyValuePair<string, int> itm in folds[i]) {
-        //        System.Console.WriteLine("Item# " + j + " - Key: " + itm.Key + ", Value: " + itm.Value);
-        //        j++;
-        //    }
-        //}
-        //wordCountDict.Clear();
-
-        //Dictionary<string, int> train = new Dictionary<string, int>();
-        //train.Add("a", 5);
-        //train.Add("b", 4);
-        //train.Add("c", 3);
-        //train.Add("d", 3);
-        //train.Add("e", 3);
-
-        //Dictionary<string, int> test = new Dictionary<string, int>();
-        //test.Add("a", 2);
-        //test.Add("b", 2);
-        //test.Add("e", 5);
-
-        //double valD = Utilities.CosineSimilarity(train, test);
-        //System.Console.WriteLine("Cosine Similarity: " + valD);
-
-
-
-        //Utilities.CountUpWords((sender as ListBox).SelectedItem as Post, wordCountDict);
-        //foreach (KeyValuePair<string, uint> itm in wordCountDict) {
-        //    System.Console.WriteLine("Key: " + itm.Key + ", Value: " + itm.Value);
-        //}
-        //wordCountDict.Clear();
-
-        //********************************Vincent's Test Code end************************/
+        private static void TestStemmer(IStemmer stemmer, params string[] words)
+        {
+            foreach (string word in words)
+            {
+                Console.WriteLine(word + " --> " + stemmer.Stem(word));
+            }
+        }
     }
 }
---
+++ SSSS/WordStopper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+public class WordStopper
+{
+    private static string[] stopWordsArrary = new string[] { "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as",
+"at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot",
+"could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each",
+"few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd",
+"he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i",
+"i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me",
+"more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or",
+"other", "ought", "our", "ours ", " ourselves", "out", "over", "own", "same", "shan't", "she", "she'd",
+"she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their",
+"theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're",
+"they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we",
+"we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's",
+"which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd",
+"you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" 
+                                            };
+
+    /// 
+    /// Removes stop words from the text.
+    /// 
+    public static string RemoveStopWords(string inputText)
+    {
+
+        inputText = inputText
+                                        .Replace("\\", string.Empty)
+                                        .Replace("|", string.Empty)
+                                        .Replace("(", string.Empty)
+                                        .Replace(")", string.Empty)
+                                        .Replace("[", string.Empty)
+                                        .Replace("]", string.Empty)
+                                        .Replace("*", string.Empty)
+                                        .Replace("?", string.Empty)
+                                        .Replace("}", string.Empty)
+                                        .Replace("{", string.Empty)
+                                        .Replace("^", string.Empty)
+                                        .Replace("+", string.Empty);
+
+        // transform given text into array of words
+        char[] wordSeparators = new char[] { ' ', '\n', '\r', ',', ';', '.', '!', '?', '-', ' ', '"', '\'' };
+        string[] words = inputText.Split(wordSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+        // Create and initializes a new StringCollection.
+        StringCollection myStopWordsCol = new StringCollection();
+        // Add a range of elements from an array to the end of the StringCollection.
+        myStopWordsCol.AddRange(stopWordsArrary);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < words.Length; i++)
+        {
+            string word = words[i].ToLowerInvariant().Trim();
+            if (word.Length > 1 && !myStopWordsCol.Contains(word))
+                sb.Append(word + " ");
+        }
+
+        return sb.ToString();
+    }
+}
